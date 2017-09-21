5a4f1833032b4537eecd587057189732afbfa930
jjmschofield <jjmschofield@googlemail.com>
2017-09-21 19:56:43 +0100

    Adding material design lite to the project

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ class App extends Component {
         return (
             <div className="app">
                 <h1>Hello World</h1>
+                <div>
+                <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                    Button
+                </button>
+                </div>
+                <div>
+                <button className="mdl-button mdl-js-button mdl-button--fab mdl-button--colored">
+                    <i className="material-icons">add</i>
+                </button>
+                </div>
             </div>
         );
     }
