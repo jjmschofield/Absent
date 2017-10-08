19e9937c66e86bb43bb3811b8305d429762dff49
jjmschofield <jjmschofield@googlemail.com>
2017-10-08 21:30:01 +0100

    Removing iso date as it is only used internally and allowing absence to be contructed with a date object

--- src/Store/Models/Absences/AbsenceModel.js
+++ src/Store/Models/Absences/AbsenceModel.js
@@ -6,13 +6,24 @@ export class Absence{
     constructor(id = null, userId, date, unit, type = AbsenceTypes.PRESENT){ //If the type is not specified we set the absence to being present as per the API data spec
         this.id = id;
         this.userId = userId;
-        this.isoDate = getIsoDateFromString(date); //TODO - ideally this should be returned from the API in a standard, timestamp, ISO or RFC2822 format
-        this.date = getDateFromIsoDate(this.isoDate);
+        this.date = getDateFromArg(date);
         this.timestamp = this.date.getTime();
         this.unit = unit;
         this.type = type;
         this.typeName = getReadableTypeName(this.type);
     }
+}
+
+function getDateFromArg(dateArg){
+    if(dateArg instanceof Date){
+        return dateArg;
+    }
+    else if(typeof dateArg === "string"){
+        let isoDateString = getIsoDateFromString(dateArg);
+        if(isoDateString.length > 0) return getDateFromIsoDate(isoDateString);
+    }
+
+    return null;
 }
 
 export default Absence;
