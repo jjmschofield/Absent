33a68e57de1303e2874ed50dd7be7004a018ba82
jjmschofield <jjmschofield@googlemail.com>
2017-10-08 16:53:28 +0100

    Givening absences an ID

--- src/Store/Models/Absences/AbsenceModel.js
+++ src/Store/Models/Absences/AbsenceModel.js
@@ -3,13 +3,14 @@ import {AbsenceTypes, getReadableTypeName} from './AbsenceTypes';
 
 
 export class Absence{
-    constructor(userId, date, unit, type){
+    constructor(id = null, userId, date, unit, type = AbsenceTypes.PRESENT){ //If the type is not specified we set the absence to being present as per the API data spec
+        this.id = id;
         this.userId = userId;
         this.isoDate = getIsoDateFromString(date); //TODO - ideally this should be returned from the API in a standard, timestamp, ISO or RFC2822 format
         this.date = getDateFromIsoDate(this.isoDate);
         this.timestamp = this.date.getTime();
         this.unit = unit;
-        this.type = type || AbsenceTypes.PRESENT; //If the type is not specified we set the absence to being present as per the API data spec
+        this.type = type;
         this.typeName = getReadableTypeName(this.type);
     }
 }
